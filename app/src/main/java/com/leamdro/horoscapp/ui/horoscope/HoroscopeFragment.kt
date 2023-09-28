f39197a7ccf9b38150becccec8cbf2ae98f5a1cb
LeandroGonzalez <leandrogonzales@wposs.com>
2023-09-28 16:12:28 -0500

    IMPLEMENTACION - Fragments para la aplicacion
    *Se implementan los fragments principales para la app

---
+++ app/src/main/java/com/leamdro/horoscapp/ui/horoscope/HoroscopeFragment.kt
@@ -0,0 +1,25 @@
+package com.leamdro.horoscapp.ui.horoscope
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.leamdro.horoscapp.databinding.FragmentHoroscopeBinding
+
+
+class HoroscopeFragment : Fragment() {
+
+    private var _binding: FragmentHoroscopeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHoroscopeBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+
+}
---
+++ app/src/main/java/com/leamdro/horoscapp/ui/palmistry/PalmistryFragment.kt
@@ -0,0 +1,25 @@
+package com.leamdro.horoscapp.ui.palmistry
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.leamdro.horoscapp.R
+import com.leamdro.horoscapp.databinding.FragmentLuckBinding
+import com.leamdro.horoscapp.databinding.FragmentPalmistryBinding
+
+class PalmistryFragment : Fragment() {
+    private var _binding: FragmentPalmistryBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentPalmistryBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+
+}
