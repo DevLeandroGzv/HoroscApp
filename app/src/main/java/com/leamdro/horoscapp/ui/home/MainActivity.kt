b68eb288039ebd5f6e85cb495b73bfae61f07757
LeandroGonzalez <leandrogonzales@wposs.com>
2023-10-03 15:30:40 -0500

    IMPLEMENTACION - Iconos y navegacion entre fragments
    *Se agrega los iconos del menu y los strings.
    *Se agrega la navegacion entre los fragments.

--- app/src/main/java/com/leamdro/horoscapp/ui/home/MainActivity.kt
+++ app/src/main/java/com/leamdro/horoscapp/ui/home/MainActivity.kt
@@ -2,14 +2,29 @@ package com.leamdro.horoscapp.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.leamdro.horoscapp.R
 import com.leamdro.horoscapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding :ActivityMainBinding
+    private lateinit var navController : NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+       InitUI();
+    }
+
+    private fun InitUI() {
+        initNavigation();
+    }
+
+    private fun initNavigation() {
+        val navHost = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHost.navController
+        binding.bottomNavView.setupWithNavController(navController)
     }
 }
